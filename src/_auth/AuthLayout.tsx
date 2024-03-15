98b8ff15594719640eabf35add107c4b3e1c1536
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-03-16 02:06:50 +1100

    divaleped a simple signupform

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,10 +1,23 @@
-
+import { Outlet, Navigate } from "react-router-dom"
 
 const AuthLayout = () => {
+
+  const isAuthenticated = false;
   return (
-    <div>
+    <>
+      {isAuthenticated ? (
+        <Navigate to='/' />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img src="../public/assets/images/side-img.png" alt="logo" className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" />
+        </>
       
-    </div>
+      )}
+    </>
   )
 }
 
